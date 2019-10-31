9fad65c6008298c3c2804b8dc5198a5486f3a137
YuriTarded12 <c.sztyk@hotmail.com>
2019-10-31 17:12:40 -0400

    Update Client.java

--- banking/src/banking/Client.java
+++ banking/src/banking/Client.java
@@ -5,10 +5,39 @@
  */
 package banking;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author cstuser
  */
-public class Client {
+public class Client implements IClient{
+    private int id;
+    private String firstName;
+    ArrayList<Account> accountList;
+    static int counter;
+    
+    
+    public Client(String firstName, int id){
+        this.firstName = firstName;
+        this.id = id;
+        counter++;
+
+
+    }
+    
+    public void addAccount(Account newAccount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public void displayAccounts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public Account getAccount(int accountNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
