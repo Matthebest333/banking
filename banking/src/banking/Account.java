96c5f4f898eacc1c9bce9769faeed0f63af64467
Matthebest333 <36921263+Matthebest333@users.noreply.github.com>
2019-11-12 17:18:11 -0500

    Update Account.java

--- banking/src/banking/Account.java
+++ banking/src/banking/Account.java
@@ -56,8 +56,6 @@ public class Account implements IAccount {
 }
 
 
-kzjhvklhgxfvjxvjhBv xh glavbdz,xgkszvhbz,jvbmdvhwe jkvnbs,hfhiljsjkvbjageuilrilvnx,bsjkfk3huihfajk
-asdfghjkl;sdfghjkl;asdfghjkl
-sdaljdf
-asfdhjk
+//Matthew wrote this
+//bleh bleh bleh
 
